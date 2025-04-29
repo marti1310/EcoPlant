6805cc6e755aae430c7448cf4d8a924ff38a8a54
alderaan <anaismackow@gmail.com>
2025-04-29 15:19:45 +0200

    DAO implemented

--- app/src/main/java/com/example/ecoplant/activities/MainActivity.java
+++ app/src/main/java/com/example/ecoplant/activities/MainActivity.java
@@ -1,4 +1,32 @@
 package com.example.ecoplant.activities;
 
-public class MainActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.ecoplant.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnSignup;
+    private Button btnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start); // <- charge ton layout XML
+
+        btnSignup = findViewById(R.id.btnSignup);
+        btnLogin = findViewById(R.id.btnLogin);
+
+        btnSignup.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+            startActivity(intent);
+        });
+
+        btnLogin.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+        });
+    }
 }
--- app/src/main/java/com/example/ecoplant/database/UtilisateurDao.java
+++ app/src/main/java/com/example/ecoplant/database/UtilisateurDao.java
@@ -1,4 +1,30 @@
 package com.example.ecoplant.database;
 
-public class UtilisateurDao {
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+import androidx.room.Delete;
+
+import com.example.ecoplant.models.Utilisateur;
+
+import java.util.List;
+
+@Dao
+public interface UtilisateurDao {
+
+    @Insert
+    void insert(Utilisateur utilisateur);
+
+    @Update
+    void update(Utilisateur utilisateur);
+
+    @Delete
+    void delete(Utilisateur utilisateur);
+
+    @Query("SELECT * FROM Utilisateurs WHERE email = :email LIMIT 1")
+    Utilisateur findByEmail(String email);
+
+    @Query("SELECT * FROM Utilisateurs")
+    List<Utilisateur> getAllUtilisateurs();
 }
