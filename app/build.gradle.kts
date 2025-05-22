37c7440fe886e0df300dc0c534a06894370bc542
Marti <martyagusan@gmail.com>
2025-05-22 15:55:06 +0200

    layouts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,7 +67,8 @@ dependencies {
     debugImplementation(libs.androidx.ui.test.manifest)
     implementation(platform("com.google.firebase:firebase-bom:33.13.0"))
     implementation("com.google.firebase:firebase-analytics")
-
+    implementation ("com.google.android.material:material:1.12.0")
+    implementation ("androidx.cardview:cardview:1.0.0")
 
 
 }
